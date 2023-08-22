a25df142786d3eeb8062a6e44fc02e1612f28168
alin <w2233@e-mirim.hs.kr>
2023-08-22 15:05:21 +0900

    스타일 다듬기, 글자 길면 ..., 책 효과, 책 애니메이션, 모바일일 때, 책 가운데로

--- 20230620_bookreviewcalender/js/review.js
+++ 20230620_bookreviewcalender/js/review.js
@@ -1,57 +1,58 @@
-// let name = "최유진"
-// console.log(`안넝, ${name}`);
-
-// URL이 있는 book parameter 값(string) 
-let url_href = window.location.href;    //브라우저 창에 있는 주
+//URL에 있는 book parameter 값(string): bookId
+let url_href = window.location.href;        //브라우저 창에 있는 주소
 const bookId = new URL(url_href).searchParams.get("book");
-console.log(`bookId: ${bookId}, type : ${typeof bookId}`);
-
-//-> String 을 number로 변환:bookIdNumber 
+console.log(`bookId: ${bookId}, type: ${typeof bookId}`);   //"3", string
+//bookId가 null이면, index.html로 가자
+if (bookId == null) {
+    window.location.href = "index.html";
+    // window.open("index.html", target="_top");    //새로운 창 열려서 이동하는 문제 해결
+}
+// -> string을 number로 변환: bookIdNumber
 let bookIdNumber = parseInt(bookId);    //string->number
-// let bookIdNumber = Number(bookId);         //string->number
-// let bookIdNumber = bookId * 1;          //string->number
-
-console.log(`bookIdNumber: ${bookIdNumber}, type : ${typeof bookIdNumber}`);  //3, number
-
-//-> books에서 하나씩 꺼내어서 bookIdNumber랑 꺼낸 book의 id랑 같으면 데이터 뺴자 :bookData
-//bookIdNumber: URL에서 가져온 id값 === books 하나씩 꺼낸 book의 id 값
+// let bookIdNumber = Number(bookId);  //string->number
+// let bookIdNumber = bookId * 1;  //string->number
+console.log(`bookIdNumber: ${bookIdNumber}, type: ${typeof bookIdNumber}`); //3, number
+// -> books에서 하나씩 꺼내어 bookIdNumber랑 꺼낸 book의 id랑 같으면 데이터 빼자: bookData
+//bookIdNumber: URL에서 가져온 id 값 === books 하나씩 꺼낸 book의 id 값
 let bookData;
-for(let book of books) {
-    if(bookIdNumber === book.id) {
-        bookData = book;    //id가 같으면, 걔를 꺼내서 bookData에 넣자
+for (let book of books) {
+    if (bookIdNumber === book.id) {
+        bookData = book;        //id가 같으면, 그놈을 꺼내서 bookData에 넣자
         break;
     }
 }
-console.log(bookData);
-
-
-//-> title, author, publisher, bookImage 알아내자
+// -> title, author, publisher, bookImage 알아내자
 let title = bookData.title;
 // let title = bookData["title"];
 let author = bookData.author;
 let publisher = bookData.publisher;
 let bookImage = bookData.img;
 console.log(title, author, publisher, bookImage);
-
-//-> HTML에 넣자
+// -> HTML에 넣자
 //HTML 요소 -> js 변수
 const titleDiv = document.getElementsByClassName("title")[1];
+titleDiv.innerHTML = `<u>${title}</u>`;
 titleDiv.innerHTML = title;
-
 const authorDiv = document.getElementsByClassName("author")[1];
 authorDiv.innerHTML = author;
-
 const publisherDiv = document.getElementsByClassName("publisher")[1];
 publisherDiv.innerHTML = publisher;
-
 const bookImageDiv = document.getElementsByClassName("book-image")[0];
-bookImageDiv.innerHTML = `<img src="${bookImage}" />`;
-
-
-
-
-//내 숙제
-let url = new URL(location.href).searchParams;
-let book = url.get("book");
-
-console.log(`bookid : ${book}`);
+bookImageDiv.innerHTML = `<img src="${bookImage}" />`
+const readDateDiv = document.getElementsByClassName("read-date")[1];
+let now = "2030년 8월 17일";
+now = new Date();   //현재 날짜와 시각 객체
+let year = now.getFullYear();
+let month = now.getMonth(); //8월이 7로 나와. return 실제 월 -1
+let date = now.getDate();
+let day = now.getDay();
+// if (day == 0) {
+//     day = "일요일";
+// }
+// else if (day == 1) {
+//     day = "월요일";
+// }
+// else if (day == 2) {
+//     day = "화요일";
+// }
+// else if (day == 3) {
