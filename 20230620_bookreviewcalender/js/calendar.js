fc5a001acfebc746024b9326f60f7ba14df79363
alin <w2233@e-mirim.hs.kr>
2023-09-14 10:21:02 +0900

    이전달, 이후달 기능 추가

--- 20230620_bookreviewcalender/js/calendar.js
+++ 20230620_bookreviewcalender/js/calendar.js
@@ -1,53 +1,72 @@
 // 이번달
-
-// 현재 날짜 / 시각 객체 만들자
+// 현재날짜, 시각 객체 만들자
 var now = new Date();
-
-// 현재 월
-var year = now.getFullYear();
 // 현재 년
+var year = now.getFullYear();
+// 현재 월
 var month = now.getMonth() + 1;
-
+// 현재 일
 var date = now.getDate();
 
-const datesContainerDiv = document.querySelector(".dates.container")
-
+const datesContainerDiv = document.querySelectorAll(".dates.container")[0];
 const setCalendar = (year, month) => {
-    // 제목달 보여주자!
+    // 제목달 보여주자
     // html -> js
-    var titleMonthDiv = document.querySelector('.month');
-    titleMonthDiv.innerHTML = `${month}월`;
+    const titleMonthDiv = document.getElementsByClassName("month")[0];
+    // .innerHTML = `${month}월`;
+    titleMonthDiv.innerHTML = `${year}년 ${month}월`;
 
-    // 달력을 만들자
-    // 1일의 요일 
+    //달력 만들자
+    //해당 월의 1일이 무슨 요일
     var firstDateDay = new Date(year, month - 1, 1).getDay();
     //해당 월의 마지막 날짜가 며칠?: 다음달의 1일 하루 전날(0일)
-
-    // 달의 마지막 일
     var lastDate = new Date(year, month - 1 + 1, 0).getDate();
 
+    //원래 있던 달력의 .date.item clear
+    datesContainerDiv.replaceChildren();
     //.date.item{$}*lastDate
-    // for 1 ~ lastDate
+    //for 1~lastDate
     for (let date = 1; date <= lastDate; date++) {
-        //<div class = "date item">date</div>
-        let dateItemDiv = document.createElement('div'); //<div></div>만드는 과정
-        dateItemDiv.classList.add("date"); // <div class= "date"></div>
-        dateItemDiv.classList.add("item"); // <div class= "date item"></div>
-        dateItemDiv.innerHTML = date;
+        //<div class="date item">date</div>
+        let dateItemDiv = document.createElement("div");    //<div></div>
+        dateItemDiv.classList.add("date");      //<div class="date"></div>
+        dateItemDiv.classList.add("item");      //<div class="date item"></div>
+        dateItemDiv.innerHTML = date;           //<div class="date item">날짜</div>
         //HTML에 .dates.container 자식으로 넣자
         datesContainerDiv.appendChild(dateItemDiv);
     }
 
-    
-    
-
-    //1일을 firstDateDay로 옮기자
-    //.dates.container의 자식 중 첫째자식(1일) style-colum-start: day +1
+    //1일을 firstDateDay로 옮기자   5 -> 6
+    //.dates.container의 자식 중 첫째자식(1일) style grid-column-start: day+1
     let firstDateDiv = datesContainerDiv.firstElementChild;
-    firstDateDiv.style.gridColumStatr = firstDateDay +1;
-
-    // < 이전달
-    // > 이후달
+    //CSS { grid-column-start: firstDateDay + 1;}
+    firstDateDiv.style.gridColumnStart = firstDateDay + 1;
 }
-
 setCalendar(year, month);
+
+// <: 이전달
+//html -> js
+const leftDiv = document.getElementsByClassName("left")[0];
+leftDiv.onclick = () => {
+    month--;
+    if (month == 0) {
+        year--;
+        month = 12;
+    }
+    // let thisMonthDateObject = new Date(year, month - 1);
+    // year = thisMonthDateObject.getFullYear();
+    // month = thisMonthDateObject.getMonth() + 1;
+    setCalendar(year, month);
+}
+// leftDiv.addEventListener("click", () => console.log(`${month}월`));
+
+// >: 이후달
+const rightDiv = document.getElementsByClassName("right")[0];
+rightDiv.onclick = () => {
+    month++;
+    if (month == 13) {
+        year++;
+        month = 1;
+    }
+    setCalendar(year, month);
+}
